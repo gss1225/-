067160a8b7dcb1a89898301b23478c886dd620ed
ScobraCk <scobrack@gmail.com>
2025-08-19 14:26:43 +0900

    testing

--- kiwoom_test.py
+++ kiwoom_test.py
@@ -1,6 +1,8 @@
 from api.kiwoom_api import KiwoomAPI
 from pprint import pprint
+from dotenv import load_dotenv
 
+load_dotenv()
 kiwoom = KiwoomAPI()
 pprint(kiwoom.get_access_token())
 pprint(kiwoom.get_stock_info('005930'))  # Example stock_code for Samsung Electronics
