2c1f4b6d99954ef77dd852450dd6df0871d6dfcd
ScobraCk <scobrack@gmail.com>
2025-08-27 14:32:41 +0900

    fix

--- api/kiwoom_api.py
+++ api/kiwoom_api.py
@@ -112,7 +112,7 @@ class KiwoomAPI:
         headers = {
             **self.headers,
             'authorization': f"Bearer {self.access_token}",
-            'api-id': 'ka10018'
+            'api-id': 'kt10018'
         }
         response = self._post(
             url,
